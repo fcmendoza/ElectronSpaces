21e8b74bcfaab46eab25a2d057152b655b8d5171
Fernando Mendoza <fcmendoza@gmail.com>
2017-02-26 17:34:22 -0800

    Added sample working countdown app.

---
+++ src/main.js
@@ -0,0 +1,37 @@
+const electron = require('electron')
+
+const countdown = require('./countdown')
+
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const ipc = electron.ipcMain
+
+let mainWindow
+
+app.on('ready', _=> {
+	mainWindow = new BrowserWindow({
+		height: 400,
+		width: 400
+	})
+
+	mainWindow.loadURL(`file://${__dirname}/countdown.html`)
+
+	//countdown()
+
+	mainWindow.on('closed', _=> {
+		console.log('closed!')
+		mainWindow = null
+	})
+
+	ipc.on('countdown-start', _=> {
+		countdown(count => {
+			//console.log('count', count)
+			mainWindow.webContents.send('countdown', count)
+		})
+
+		// countdown(function(count) {
+		// 	console.log('count', count)
+		// 	mainWindow.webContents.send('countdown', count)
+		// })
+	})
+})
